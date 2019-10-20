840dc61753bd78e221eb664f258f1323968bb0ef
thepnarin <43739993+thepnarin@users.noreply.github.com>
2019-10-20 13:24:28 +0700

    TestingAllTheExecutionTests

---
+++ 1_8_TestingAllTheExecutionTests/Fundamentals/Reservation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_8_TestingAllTheExecutionTests.Fundamentals
+{
+    public class Reservation
+    {
+        public User MadeBy { get; set; }
+
+        public bool CanBeCancelledBy(User user)
+        {
+            if (user.IsAdmin)
+                return true;
+            if (MadeBy == user)
+                return true;
+
+            return false;
+        }
+
+    }
+
+    public class User
+    {
+        public bool IsAdmin { get; set; }
+    }
+}
